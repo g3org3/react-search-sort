ec1d273c908f596b59fecf1df3edfec81f096dfd
george <george@macbook-2.local>
2018-07-24 17:58:52 +0200

    initial commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'react-search-sort'
+import Sort from 'react-search-sort'
+import data from './users.json'
+import Table from 'react-bootstrap-gtable'
 
 export default class App extends Component {
   render () {
+    const { users } = data
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <Sort initialData={users}>
+          {({ data, setSortBy, onSearchInput }) => (
+            <div>
+              <div className="form-group">
+                <input
+                  className="form-control"
+                  name="search"
+                  placeholder="search"
+                  onChange={onSearchInput}
+                />
+              </div>
+              <Table data={data} onThClick={setSortBy} />
+            </div>
+          )}
+        </Sort>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,75 @@
-import React, { Component } from 'react'
+import { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 
-import styles from './styles.css'
+export default class SearchAndSortArray extends PureComponent {
+  propTypes = {
+    initialData: PropTypes.array,
+    children: PropTypes.func.isRequired
+  }
+  static defaultProps = {
+    initialData: []
+  };
+  state = {
+    data: [...this.props.initialData],
+    search: '',
+    sortBy: '',
+    asc: true
+  };
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+  sortFn(name, asc) {
+    return (a, b) => (asc ? a[name] > b[name] : a[name] < b[name])
+  }
+
+  setSortBy = (name = '') => {
+    const { initialData } = this.props
+    this.setState(s => ({
+      ...s,
+      sortBy: name,
+      asc: s.sortBy === name ? !s.asc : s.asc,
+      data: initialData.sort(
+        this.sortFn(name, s.sortBy === name ? !s.asc : s.asc)
+      )
+    }))
+  };
+
+  onSearch = (value = '') => {
+    const { initialData } = this.props
+    const { keys } = Object
+    this.setState(s => ({
+      ...s,
+      search: value,
+      data: initialData.filter(
+        item =>
+          keys(item).filter(
+            k => `${item[k]}`.toLowerCase().indexOf(value) !== -1
+          ).length
+      )
+    }))
+  };
+
+  onSearchInput = ({ target = {} } = {}) => {
+    const value = target.value || ''
+    return this.onSearch(value)
+  }
+
+  removeSearch = () => {
+    const { initialData } = this.props
+    this.setState({
+      data: [...initialData]
+    })
+  };
+
+  getProps() {
+    return {
+      data: this.state.data,
+      setSortBy: this.setSortBy,
+      onSearch: this.onSearch,
+      removeSearch: this.removeSearch,
+      onSearchInput: this.onSearchInput
+    }
   }
 
   render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+    return this.props.children(this.getProps())
   }
 }
